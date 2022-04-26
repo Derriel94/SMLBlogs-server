ea270c48061e5f7082f9450c7c62c5b7e281a9d0
Derriel Collins <derrielcollins96@gmail.com>
2022-04-26 11:00:30 -0500

    fixed backend issue for image download

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors')
-
+const fileUpload = require('express-fileupload');
 const app = express();
-
+const path = require('path');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload({
+	useTempFiles: true,
+	tempFileDir : path.join(__dirname, 'tmp'),
+}));
 
-const database = { };
 
 app.get('/', (req, res)=> {
 	res.send('Home Son!')
@@ -16,8 +19,26 @@ app.get('/', (req, res)=> {
 });
 
 app.post('/editor', (req, res)=> {
-	
+	const { textArea, blogTitle } = req.body;
+	console.log(textArea, blogTitle);
+	res.send("we got the goods");
 
+});
+app.post('/upload', (req, res)=> {
+	const { formData } = req.files.file;
+	console.log(req.files.file);
+	if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    // Accessing the file by the <input> File name="t_file"
+    let targetFile = req.files.file;
+    //mv(path, CB function(err))
+    targetFile.mv(path.join(__dirname, 'uploads', targetFile.name), (err) => {
+        if (err){
+            return res.status(500).send(err);
+        }
+        res.send('File uploaded!');
+    });
 
 });
 
