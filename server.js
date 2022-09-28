533d43a2dfc1c7ffc14ca38a065221f17d25bad6
Derriel Collins <derrielcollins96@gmail.com>
2022-09-28 16:10:39 -0500

    minor change

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ var con = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  insecureAuth : true
+  port: 3306,
 });
 
 //87170e0d
